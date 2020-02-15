84a0594e4aed10724003d234f6cde0b54584ead3
fernando_f10 <fernando_f10@outlook.com>
2020-02-15 13:50:15 -0300

    Searching movies by title, using OMDB API
    User inputs the title of the movie.
    The program uses the OMDB API to get the main information about movies containing the aforementioned title.
    
    Using API 'By Search' method:
    Example:
    http://www.omdbapi.com/?s=%22Lucy%22&apikey=#####
    Where ##### is your API
    
    See http://www.omdbapi.com/ for more information

--- source_code/imdb_movie.py
+++ source_code/imdb_movie.py
@@ -1,8 +1,30 @@
 import pickle
+import requests
+
 
 def load_api_key():
+    """Return the API string stored in key.pkl."""
     file = open('key.pkl', 'rb')
     key = pickle.load(file)
-    print(key)
+    return key
 
+
+# omdbapi API key
 key = load_api_key()
+# url pattern to search movies by title
+url_movies_search = "http://www.omdbapi.com/?s=\"{}\"&apikey=" + key
+
+movie_title = input("Movie title to search: ")
+try:
+    resp = requests.get(url_movies_search.format(movie_title))
+except:
+    print('Something went wrong')
+else:
+    movies_list = resp.json()['Search']
+    for movie_dict in movies_list:
+        title = movie_dict['Title']
+        year = movie_dict['Year']
+        imdbID = movie_dict['imdbID']
+        movie_type = movie_dict['Type']
+        poster_url = movie_dict['Poster']
+        print(f'{title} - {year} - {movie_type} - {imdbID} - {poster_url}')
