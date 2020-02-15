e5808df3f06d97fcde35224c0757aa52a19ad2bc
fernando_f10 <fernando_f10@outlook.com>
2020-02-15 16:16:00 -0300

    Added new search feature
    This feature prompts a message asking the user if he/she wants to perform a new search.
    While the response the response for this kind of question is yes, the program performs a new search.
    Else, the program exits.
    
    Modifications:
    - Moved properly code lines to the function new_search().
    - Prompts the question in the main program, which calls the new_search function or not, deppending on user response.

--- source_code/imdb_movie.py
+++ source_code/imdb_movie.py
@@ -32,32 +32,38 @@ def get_movie_info(imdbID):
         synopsis = movie_dict['Plot']
         print(f'\n{title} - {year} - {movie_type} - {imdbID} - {poster_url} - {synopsis}')
 
-
-movie_title = input("Movie title to search: ")
-try:
-    resp = requests.get(url_movies_search.format(movie_title))
-except:
-    print('Something went wrong')
-else:
-    dic = resp.json()
-    if dic['Response'] == 'True':
-        movies_list = dic['Search']
-        current_movie_index = 0
-        while current_movie_index != 'exit':
-            movie = movies_list[current_movie_index]
-            imdbID = movie['imdbID']
-            get_movie_info(imdbID)
-            previous_next_movie = input('Choose next or prev: ')
-            if previous_next_movie == 'next':  # goes to the next movie
-                current_movie_index += 1
-                if current_movie_index >= len(movies_list):  # goes to the list begginig
-                    current_movie_index = 0
-            elif previous_next_movie == 'prev':  # goes to the previous movie
-                current_movie_index -= 1
-                if current_movie_index < 0:  # goes to the end of the list
-                    current_movie_index = len(movies_list) - 1
-            else:
-                print('Exiting...')
-                current_movie_index = 'exit'
+def new_search():
+    movie_title = input("Movie title to search: ")
+    try:
+        resp = requests.get(url_movies_search.format(movie_title))
+    except:
+        print('Something went wrong')
     else:
-        print('No movie found with this title')
+        dic = resp.json()
+        if dic['Response'] == 'True':
+            movies_list = dic['Search']
+            current_movie_index = 0
+            while current_movie_index != 'exit':
+                movie = movies_list[current_movie_index]
+                imdbID = movie['imdbID']
+                get_movie_info(imdbID)
+                previous_next_movie = input('Choose next or prev: ')
+                if previous_next_movie == 'next':  # goes to the next movie
+                    current_movie_index += 1
+                    if current_movie_index >= len(movies_list):  # goes to the list begginig
+                        current_movie_index = 0
+                elif previous_next_movie == 'prev':  # goes to the previous movie
+                    current_movie_index -= 1
+                    if current_movie_index < 0:  # goes to the end of the list
+                        current_movie_index = len(movies_list) - 1
+                else:
+                    print('Exiting...')
+                    current_movie_index = 'exit'
+        else:
+            print('No movie found with this title')
+
+
+search = 'yes'
+while search == 'yes':
+    new_search()
+    search = input('Do you want to do a new search(yes/not)? ')
