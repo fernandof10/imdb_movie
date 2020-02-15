a9b0481a79835f63b43467bbf502b9381379217f
fernando_f10 <fernando_f10@outlook.com>
2020-02-15 15:50:25 -0300

    Added next/previous movie feature
    After showing the first movie info, the program asks to the user if he/she wants to move to the next movie.
    The user navigates cyclically, meaning that if the user is checkint the latest movie and select 'next', the list goes to the beginnig.
    The same for the 'prev' case.
    If user types anything different from above cases, the program exits.

--- source_code/imdb_movie.py
+++ source_code/imdb_movie.py
@@ -42,8 +42,22 @@ else:
     dic = resp.json()
     if dic['Response'] == 'True':
         movies_list = dic['Search']
-        for movie_dict in movies_list:
-            imdbID = movie_dict['imdbID']
+        current_movie_index = 0
+        while current_movie_index != 'exit':
+            movie = movies_list[current_movie_index]
+            imdbID = movie['imdbID']
             get_movie_info(imdbID)
+            previous_next_movie = input('Choose next or prev: ')
+            if previous_next_movie == 'next':  # goes to the next movie
+                current_movie_index += 1
+                if current_movie_index >= len(movies_list):  # goes to the list begginig
+                    current_movie_index = 0
+            elif previous_next_movie == 'prev':  # goes to the previous movie
+                current_movie_index -= 1
+                if current_movie_index < 0:  # goes to the end of the list
+                    current_movie_index = len(movies_list) - 1
+            else:
+                print('Exiting...')
+                current_movie_index = 'exit'
     else:
         print('No movie found with this title')
